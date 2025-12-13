cb7a85595e21a34e53cc049de6f5a8a71559f1af
Mirkomil Mirvohidov <mirkomilmirvohidov4@gmail.com>
2025-12-14 00:02:29 +0500

    sql

--- config/settings.py
+++ config/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'one_db',      # Your DB name
+        'USER': 'postgres',          # Your DB user
+        'PASSWORD': '123',  # Your DB password
+        'HOST': 'localhost',       # Use 'localhost' or your server IP
+        'PORT': '5432',            # Default PostgreSQL port
     }
 }
 
